94abff0bae1df8b3819b30b74a60b54645eb88ae
Antony Watkins <arwatkins89@gmail.com>
2020-11-24 15:53:11 -0500

    added all variables and functions needed for game

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,25 @@
 // All code should be written in this file.
+
+const playerOneMoveOneType = 
+const playerOneMoveOneValue = 
+const playerOneMoveTwoType = 
+const playerOneMoveTwoValue =
+const playerOneMoveThreeType =
+const playerOneMoveThreeValue = 
+
+const playerTwoMoveOneType = 
+const playerTwoMoveOneValue = 
+const playerTwoMoveTwoType =
+const playerTwoMoveTwoValue = 
+const playerTwoMoveThreeType = 
+const playerTwoMoveThreeValue = 
+
+const setPlayerMoves = (player, moveOneType, MoveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+
+};
+
+const getRoundWinner = (roundNumber) => {
+
+};
+
+const getGameWinner = ()
