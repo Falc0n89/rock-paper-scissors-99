028cb73ab5d54fee920290cc2c9b2e80b775ab5c
Antony Watkins <arwatkins89@gmail.com>
2020-11-26 19:51:59 -0500

    added main variables and set up functions for edge cases with tests passed

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,25 +1,63 @@
 // All code should be written in this file.
+let playerOneMoveOneType,
+    playerOneMoveOneValue,
+    playerOneMoveTwoType,  
+    playerOneMoveTwoValue, 
+    playerOneMoveThreeType, 
+    playerOneMoveThreeValue,  
 
-const playerOneMoveOneType = 
-const playerOneMoveOneValue = 
-const playerOneMoveTwoType = 
-const playerOneMoveTwoValue =
-const playerOneMoveThreeType =
-const playerOneMoveThreeValue = 
+    playerTwoMoveOneType,
+    playerTwoMoveOneValue,
+    playerTwoMoveTwoType,
+    playerTwoMoveTwoValue,  
+    playerTwoMoveThreeType,  
+    playerTwoMoveThreeValue; 
 
-const playerTwoMoveOneType = 
-const playerTwoMoveOneValue = 
-const playerTwoMoveTwoType =
-const playerTwoMoveTwoValue = 
-const playerTwoMoveThreeType = 
-const playerTwoMoveThreeValue = 
+// The method signature for this function should be as follows: setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue)
 
-const setPlayerMoves = (player, moveOneType, MoveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+    //create failfast return if values not provided
+    if(!moveOneType || !moveOneValue || !moveTwoType || !moveTwoValue || !moveThreeType || !moveThreeValue) {
+        return;
+    }
 
-};
+    if(!isValidMoveType(moveOneType) || !isValidMoveType(moveTwoType) || !isValidMoveType(moveThreeType)) {
+        return;
+    }
 
-const getRoundWinner = (roundNumber) => {
+    if(!isValidMoveValue(moveOneValue) || !isValidMoveValue(moveTwoValue) || !isValidMoveValue(moveThreeValue)) {
+        return;
+    }
 
-};
+    if ((moveOneValue + moveTwoValue + moveThreeValue) > 99) {
+        return;
+    }
 
-const getGameWinner = ()
+    if (player === 'Player One') {
+        playerOneMoveOneType = moveOneType;
+        playerOneMoveOneValue = moveOneValue;
+        playerOneMoveTwoType = moveTwoType;
+        playerOneMoveTwoValue = moveTwoValue;
+        playerOneMoveThreeType = moveThreeType;
+        playerOneMoveThreeValue = moveThreeValue;
+    } else if (player === 'Player Two') {
+        playerTwoMoveOneType = moveOneType;
+        playerTwoMoveOneValue = moveOneValue;
+        playerTwoMoveTwoType = moveTwoType;
+        playerTwoMoveTwoValue = moveTwoValue;
+        playerTwoMoveThreeType = moveThreeType;
+        playerTwoMoveThreeValue = moveThreeValue;
+    }
+}
+
+//Create helper function 'isValidMoveType()' with parameter 'moveType' to validate value of rock paper or scissors
+function isValidMoveType(moveType) {
+    return (moveType === 'rock') || (moveType === 'paper') || (moveType === 'scissors');
+}
+
+//create helper function for the value too
+
+function isValidMoveValue(moveValue) {
+    return (moveValue >= 1) && (moveValue <=99);
+}
+
