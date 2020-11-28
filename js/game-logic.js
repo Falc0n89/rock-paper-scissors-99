fde474462ff66094449bc26e575570c115830940
Antony Watkins <arwatkins89@gmail.com>
2020-11-28 03:46:20 -0500

    all tests passed

--- js/game-logic.js
+++ js/game-logic.js
@@ -11,7 +11,9 @@ let playerOneMoveOneType,
     playerTwoMoveTwoType,
     playerTwoMoveTwoValue,  
     playerTwoMoveThreeType,  
-    playerTwoMoveThreeValue; 
+    playerTwoMoveThreeValue,
+    playerOneWins,
+    playerTwoWins; 
 
 // The method signature for this function should be as follows: setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue)
 
@@ -110,5 +112,58 @@ function getMoveWinner (playerOneMoveType, playerOneMoveValue, playerTwoMoveType
             }
         }
 
+}
 
+function getGameWinner() {
+    if (!playerOneMoveOneType || !playerOneMoveOneValue ||
+        !playerOneMoveTwoType || !playerOneMoveTwoValue ||
+        !playerOneMoveThreeType || !playerOneMoveThreeValue ||
+        !playerTwoMoveOneType || !playerTwoMoveOneValue ||
+        !playerTwoMoveTwoType || !playerTwoMoveTwoValue ||
+        !playerTwoMoveThreeType || !playerTwoMoveThreeValue) {
+            return null;
+        }
+
+        playerOneWins = 0;
+        playerTwoWins = 0;
+
+        const roundOneWinner = getRoundWinner(1);
+        const roundTwoWinner = getRoundWinner(2);
+        const roundThreeWinner = getRoundWinner(3);
+
+        addWin(roundOneWinner);
+        addWin(roundTwoWinner);
+        addWin(roundThreeWinner);
+
+        if (playerOneWins > playerTwoWins) {
+            return 'Player One';
+        } else if (playerOneWins < playerTwoWins) {
+            return 'Player Two';
+        } else {
+            return 'Tie';
+        }
+
+}
+
+function addWin(winner) {
+    if (winner === 'Player One') {
+        playerOneWins = (playerOneWins + 1) || 1;
+    } else if (winner === 'Player Two') {
+        playerTwoWins = (playerTwoWins + 1) || 1;
+    }
+}
+
+
+function setComputerMoves() {
+    const moves = ['rock', 'paper', 'scissors'];
+
+    const moveOneType = moves[Math.floor(Math.random() * 3)];
+    const moveTwoType = moves[Math.floor(Math.random() * 3)];
+    const moveThreeType = moves[Math.floor(Math.random() * 3)];
+
+    const moveOneValue = Math.floor(Math.random() * 96) + 1;
+    const moveTwoValue = Math.floor(Math.random() * (97 - moveOneValue)) + 1;
+    const moveThreeValue = 99 - moveOneValue - moveTwoValue;
+
+    setPlayerMoves('Player Two', moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue);
 }
